a270731538b19d055208006fd7d89055aa719071
esther <ogbonnaesther52@gmail.com>
2025-09-03 09:36:19 +0100

    add urls script

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,16 +1,14 @@
 from django.urls import path
-from django.contrib import admin
-from django.urls import path, include
 from .views import list_books
 from .views import LibraryDetailView
+from .views import register, CustomLoginView, CustomLogoutView
 
 urlpatterns = [
-    path('books/', views.list_books, name='list_books'),
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-]
+    path('books/', list_books, name='list_books'),
+    path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('relationship_app.urls')),
+    # Authentication URLs
+    path('register/', register, name='register'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
 ]
-
