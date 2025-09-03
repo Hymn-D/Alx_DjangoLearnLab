b7039293cf91717134fdf27082a1b277f19fb1fc
esther <ogbonnaesther52@gmail.com>
2025-09-03 09:33:22 +0100

    add views script

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
 from .views import list_books
 from .views import LibraryDetailView
 
@@ -6,3 +8,9 @@ urlpatterns = [
     path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
 ]
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('relationship_app.urls')),
+]
+
