24609351027886b75f801f82a7ba1929db2e196a
esther <ogbonnaesther52@gmail.com>
2025-09-12 00:43:29 +0100

    add library project

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,5 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.conf import settings
+
+class Book(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
 class Book(models.Model):
     title = models.CharField(max_length=200)
