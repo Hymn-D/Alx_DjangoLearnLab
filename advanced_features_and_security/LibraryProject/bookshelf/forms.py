5c9c05da27faa73a0bbef11f4b814cfe06a9f6f6
esther <ogbonnaesther52@gmail.com>
2025-09-12 03:56:21 +0100

    add bookshelf

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -1,4 +1,6 @@
 from django import forms
 
-class BookSearchForm(forms.Form):
-    query = forms.CharField(max_length=100, required=True)
+class ExampleForm(forms.Form):
+    title = forms.CharField(max_length=255, required=True)
+    author = forms.CharField(max_length=255, required=True)
+
