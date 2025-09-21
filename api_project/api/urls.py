f444fbef6510596f907be053b01abbfb48f30dfc
esther <ogbonnaesther52@gmail.com>
2025-09-21 18:27:01 +0100

    add api priject

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import path, include
 from .views import BookList
 from rest_framework.routers import DefaultRouter
 from .views import BookViewSet
-
+from rest_framework.authtoken.views import obtain_auth_token
 
 router = DefaultRouter()
 router.register(r'books', BookViewSet, basename='book_all')
@@ -13,4 +13,9 @@ urlpatterns = [
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'),  
+]
+
+urlpatterns = [
+     path('auth/token/', obtain_auth_token, name='api_token_auth'),
+  
 ]
