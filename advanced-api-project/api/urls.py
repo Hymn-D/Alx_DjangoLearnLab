e8486a3d49776a9ce078d27c416cd78574110e98
esther <ogbonnaesther52@gmail.com>
2025-09-28 14:46:37 +0100

    add advanced api projects

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     path('books/list', BookListView.as_view(), name='book-list'),
     path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('books/update/', BookUpdateView.as_view(), name='book-update'),
-    path('books/<int:pk>/delete/', BookDeleteView.as_view(), name='book-delete'),
+    path('books/delete/', BookDeleteView.as_view(), name='book-delete'),
 ]
 
 urlpatterns = [
