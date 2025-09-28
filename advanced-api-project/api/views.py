9b3ab9220127b8708244f97e27e4b11a473339bc
esther <ogbonnaesther52@gmail.com>
2025-09-28 15:15:37 +0100

    add advanced api projects

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from .models import Book
 from .serializers import BookSerializer
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
@@ -8,6 +9,15 @@ class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    filter_backends = [DjangoFilterBackend,
+                       filters.SearchFilter,
+                       filters.OrderingFilter
+    ]
+    filterset_fields = ['title', 'author', 'publication_year']
+    search_fields = ['title', 'author__name']
+    ordering_fields = ['title', 'publication_year']
+    ordering = ['title']  # Default ordering
+    
 
 class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
@@ -27,17 +37,6 @@ class BookDeleteView(generics.DestroyAPIView):
     permission_classes = [IsAuthenticated]
 
 
-# GET all books, POST new book
-class BookListCreateView(generics.ListCreateAPIView):
-    queryset = Book.objects.all()
-    serializer_class = BookSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]  # Auth required for POST
-    filter_backends = [filters.SearchFilter, filters.OrderingFilter]
-    search_fields = ['title', 'publication_year']  # enable ?search=
-    ordering_fields = ['publication_year', 'title']  # enable ?ordering=
-
-def perform_create(self, serializer): 
-    serializer.save()
 
 # GET one book, PUT/PATCH update, DELETE book
 class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
