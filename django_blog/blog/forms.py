643782e4972a3b4a05b1486e9bc84a5cd8b0e986
esther <ogbonnaesther52@gmail.com>
2025-10-11 20:40:57 +0100

    add django blog

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -3,7 +3,7 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.forms import ModelForm
 
-class UserRegisterForm(UserCreationForm):
+class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
     class Meta:
@@ -11,11 +11,9 @@ class UserRegisterForm(UserCreationForm):
         fields = ['username', 'email', 'password1', 'password2']
 
 
-class UserUpdateForm(ModelForm):
+class CustomUserCreationForm(ModelForm):
     email = forms.EmailField(required=True)
-    first_name = forms.CharField(required=False)
-    last_name = forms.CharField(required=False)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name']
+        fields = ['username', 'email']
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,71 +1,29 @@
-from django.shortcuts import render
-from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth.forms import AuthenticationForm
-from django.contrib import messages
-from .forms import UserRegisterForm, UserUpdateForm
+from django.contrib.auth.models import User
+from django import forms
+from .forms import CustomUserCreationForm
 
-
-# Register View
-def register_view(request):
+def register(request):
     if request.method == 'POST':
-        form = UserRegisterForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            messages.success(request, 'Registration successful!')
-            return redirect('profile')
-        else:
-            messages.error(request, 'Please correct the errors below.')
-    else:
-        form = UserRegisterForm()
-    return render(request, 'blog/register.html', {'form': form})
-
-
-# Login View
-def login_view(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(request, data=request.POST)
-        if form.is_valid():
-            username = form.cleaned_data['username']
-            password = form.cleaned_data['password']
-            user = authenticate(username=username, password=password)
-            if user:
-                login(request, user)
-                messages.success(request, f'Welcome back, {username}!')
-                return redirect('profile')
-            else:
-                messages.error(request, 'Invalid username or password.')
-        else:
-            messages.error(request, 'Invalid username or password.')
-    else:
-        form = AuthenticationForm()
-    return render(request, 'blog/login.html', {'form': form})
-
-
-# Logout View
-def logout_view(request):
-    logout(request)
-    messages.info(request, 'You have been logged out.')
-    return redirect('login')
-
-
-# Profile View
-@login_required
-def profile_view(request):
-    return render(request, 'blog/profile.html')
-
-
-# Profile Edit View
-@login_required
-def edit_profile_view(request):
-    if request.method == 'POST':
-        form = UserUpdateForm(request.POST, instance=request.user)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, 'Profile updated successfully!')
-            return redirect('profile')
+            return redirect('login')
     else:
-        form = UserUpdateForm(instance=request.user)
-    return render(request, 'blog/edit_profile.html', {'form': form})
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
 
+class ProfileForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+def profile(request):
+    if request.method == 'POST':
+        form = ProfileForm(request.POST, instance=request.user)
+        if form.is_valid():
+            form.save()
+    else:
+        form = ProfileForm(instance=request.user)
+    return render(request, 'blog/profile.html', {'form': form})
