44080d6a581a8b1e45d7083939b29c0b012ad4dc
esther <ogbonnaesther52@gmail.com>
2025-10-12 16:44:23 +0100

    add django blog

--- django_blog/blog/models.py
+++ django_blog/blog/models.py
@@ -1,13 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from taggit.managers import TaggableManager
+
 
 class Post(models.Model):
     title = models.CharField(max_length=200)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='posts')
-    tags = models.ManyToManyField('Tag', blank=True, related_name='posts')
+    tags = TaggableManager()
 
 
     def __str__(self):
@@ -32,4 +34,12 @@ class Tag(models.Model):
 
     def __str__(self):
         return self.name
+    
+class PostTag(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+    added_on = models.DateTimeField(auto_now_add=True)  # optional
 
+
+    def __str__(self):
+        return f"{self.post.title} - {self.tag.name}"
