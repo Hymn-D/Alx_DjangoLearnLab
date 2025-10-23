64b6c402e98a939169c6ff5e3f69369ba8b035b7
ogbonna esther <ogbonnajunia12@gmail.com>
2025-10-23 14:35:01 +0100

    Fix list_books view reference in URLs

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('add_book/', views.add_book, name='add_book'), 
     path('edit_book/<int:book_id>/', views.edit_book, name='edit_book'),  
     path('delete_book/<int:book_id>/', views.delete_book, name='delete_book'),
-    path('books/', list_books, name='list_books'),
+    path('books/', views.list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
 ]
 
