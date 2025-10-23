090e372178a37df6255f333516bd6f6a28b33648
ogbonna esther <ogbonnajunia12@gmail.com>
2025-10-23 14:12:28 +0100

    Add Library and Librarian models

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -21,4 +21,17 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.OneToOneField(Library, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.name} ({self.library.name})"
 
