81ae26cd3afdea91b5e213a7666d6e5f01c9637b
ogbonna esther <ogbonnajunia12@gmail.com>
2025-10-23 14:44:43 +0100

    Add registration and authentication URLs

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from . import views
-from .views import list_books, LibraryDetailView
+from .views import list_books, LibraryDetailView, register
 
 urlpatterns = [
     path('add_book/', views.add_book, name='add_book'), 
@@ -8,6 +8,9 @@ urlpatterns = [
     path('delete_book/<int:book_id>/', views.delete_book, name='delete_book'),
     path('books/', list_books, name='list_books'),
     path('library/<int:pk>/', LibraryDetailView.as_view(), name='library_detail'),
+    path('register/', register, name='register'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
 ]
 
 
